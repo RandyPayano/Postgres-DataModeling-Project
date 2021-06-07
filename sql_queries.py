d7a0ce7b2ec8e2fd6103901d06ed8ce3c45499bc
randypayano@gmail.com <randypayano@gmail.com>
2021-06-07 19:39:55 -0400

    tables added to queries

--- sql_queries.py
+++ sql_queries.py
@@ -1,26 +1,57 @@
 # DROP TABLES
 
-songplay_table_drop = "DROP TABLE IF EXISTS SONGPLAY;"
-user_table_drop = "DROP TABLE IF EXISTS USER;"
-song_table_drop = "DROP TABLE IF EXISTS SONG"
-artist_table_drop = "DROP TABLE IF EXIST ARTIST;"
+songplay_table_drop = "DROP TABLE IF EXISTS SONGPLAYS;"
+user_table_drop = "DROP TABLE IF EXISTS USERS;"
+song_table_drop = "DROP TABLE IF EXISTS SONGS"
+artist_table_drop = "DROP TABLE IF EXIST ARTISTS;"
 time_table_drop = "DROP TABLE IF EXIST TIME;"
 
 # CREATE TABLES
 
-songplay_table_create = ("""
+songplay_table_create = ("""CREATE TABLE SONGPLAYS IF NOT EXISTS(
+    songplay_id int,
+    start_time BIGINT,
+    user_id int, 
+    level varchar, 
+    song_id int, 
+    artist_id int, 
+    session_id int, 
+    location varchar, 
+    user_agent varchar
+)
 """)
 
-user_table_create = ("""
+user_table_create = ("""CREATE TABLE USERS IF NOT EXISTS(
+    user_id int,
+    first_name varchar,
+    last_name varchar,
+    gender varchar,
+    level varchar 
+)
 """)
 
 song_table_create = ("""
 """)
 
-artist_table_create = ("""
+artist_table_create = ("""CREATE TABLE SONGS IF NOT EXISTS(
+    song_id int,
+    title varchar,
+    artist_id int,
+    year int,
+    duration FLOAT
+)
 """)
 
-time_table_create = ("""
+time_table_create = ("""CREATE TABLE TIME IF NOT EXISTS(
+    start_time int,
+    hour int,
+    day int,
+    week int,
+    month int,
+    year int,
+    weekday varchar
+)
+
 """)
 
 # INSERT RECORDS
