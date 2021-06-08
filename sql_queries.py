382742f38ef31dcaf9c160a1922872026efa93b5
randypayano@gmail.com <randypayano@gmail.com>
2021-06-08 14:56:20 -0400

    create tables queries done

--- sql_queries.py
+++ sql_queries.py
@@ -9,47 +9,53 @@ time_table_drop = "DROP TABLE IF EXIST TIME;"
 # CREATE TABLES
 
 songplay_table_create = ("""CREATE TABLE SONGPLAYS IF NOT EXISTS(
-    songplay_id int,
+    songplay_id SERIAL PRIMARY KEY,
     start_time BIGINT,
     user_id int, 
     level varchar, 
-    song_id int, 
-    artist_id int, 
+    song_id varchar, 
+    artist_id varchar, 
     session_id int, 
     location varchar, 
     user_agent varchar
-)
+);
 """)
 
 user_table_create = ("""CREATE TABLE USERS IF NOT EXISTS(
-    user_id int,
+    user_id int PRIMARY KEY,
     first_name varchar,
     last_name varchar,
     gender varchar,
     level varchar 
-)
+);
 """)
 
-song_table_create = ("""
-""")
-
-artist_table_create = ("""CREATE TABLE SONGS IF NOT EXISTS(
-    song_id int,
-    title varchar,
-    artist_id int,
+song_table_create =  ("""CREATE TABLE SONGS IF NOT EXISTS(
+    song_id varchar NOT NULL PRIMARY KEY,
+    title varchar(255) NOT NULL,
+    artist_id varchar(255) NOT NULL,
     year int,
     duration FLOAT
+);
+""")
+
+artist_table_create = ("""CREATE TABLE ARTISTS IF NOT EXISTS(
+    artist_id varchar(255) PRIMARY KEY,
+    name varchar(255) NOT NULL,
+    location varchar,
+    latitude FLOAT,
+    longitude FLOAT
 )
 """)
 
 time_table_create = ("""CREATE TABLE TIME IF NOT EXISTS(
-    start_time int,
+    start_time BIGINT PRIMARY KEY,
     hour int,
     day int,
     week int,
     month int,
     year int,
-    weekday varchar
+    weekday int
 )
 
 """)
