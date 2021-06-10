4ba24411ac305e4dc74746976e416dfcb47658dc
randypayano@gmail.com <randypayano@gmail.com>
2021-06-09 21:36:47 -0400

    tables created, rows inserted, tested

--- create_tables.py
+++ create_tables.py
@@ -1,7 +1,9 @@
 import psycopg2
 from sql_queries import create_table_queries, drop_table_queries
-
-
+try:
+    conn.close() 
+except:
+    pass
 def create_database():
     """
     - Creates and connects to the sparkifydb
@@ -9,21 +11,21 @@ def create_database():
     """
     
     # connect to default database
-    conn = psycopg2.connect("host=127.0.0.1 dbname=studentdb user=student password=student")
+    conn = psycopg2.connect("host=127.0.0.1 dbname=student_randy user=postgres password=datapassword")
     conn.set_session(autocommit=True)
     cur = conn.cursor()
-    
+    print("connecting to student_randy db")
     # create sparkify database with UTF8 encoding
-    cur.execute("DROP DATABASE IF EXISTS sparkifydb")
-    cur.execute("CREATE DATABASE sparkifydb WITH ENCODING 'utf8' TEMPLATE template0")
-
+    cur.execute("DROP DATABASE IF EXISTS sparkifydb2")
+    cur.execute("CREATE DATABASE sparkifydb2 WITH ENCODING 'utf8' TEMPLATE template0")
+    print("sparkifydb created")
     # close connection to default database
     conn.close()    
     
     # connect to sparkify database
-    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb2 user=postgres password=datapassword")
     cur = conn.cursor()
-    
+    print("connected to sparkifydb")
     return cur, conn
 
 
@@ -41,6 +43,7 @@ def create_tables(cur, conn):
     Creates each table using the queries in `create_table_queries` list. 
     """
     for query in create_table_queries:
+        print(query)
         cur.execute(query)
         conn.commit()
 
